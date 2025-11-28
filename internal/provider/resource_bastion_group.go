2434a0ef641f53b8e34fee3ab38eb58054fe5373
jon4hz <me@jon4hz.io>
2025-11-28 14:31:09 +0100

    fix: use state for group member type fields

--- internal/provider/resource_bastion_group.go
+++ internal/provider/resource_bastion_group.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -102,21 +103,33 @@ func (r *GroupResource) Schema(ctx context.Context, req resource.SchemaRequest, 
 				ElementType:         types.StringType,
 				MarkdownDescription: "The owners of the Bastion group",
 				Computed:            true,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"members": schema.ListAttribute{
 				ElementType:         types.StringType,
 				MarkdownDescription: "The members of the Bastion group",
 				Computed:            true,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"gatekeepers": schema.ListAttribute{
 				ElementType:         types.StringType,
 				MarkdownDescription: "The gatekeepers of the Bastion group",
 				Computed:            true,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"aclkeepers": schema.ListAttribute{
 				ElementType:         types.StringType,
 				MarkdownDescription: "The ACL keepers of the Bastion group",
 				Computed:            true,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.UseStateForUnknown(),
+				},
 			},
 		},
 	}
