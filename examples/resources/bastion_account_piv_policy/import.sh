2b9b124f24454cc6083b5fcfbdddaebc67afd941
jon4hz <me@jon4hz.io>
2025-11-30 23:28:34 +0100

    feat: configure account piv policy

---
+++ examples/resources/bastion_account_piv_policy/import.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+terraform import bastion_account_piv_policy.example example-account
---
+++ examples/resources/bastion_account_piv_policy/resource.tf
@@ -0,0 +1,4 @@
+resource "bastion_account_piv_policy" "example" {
+  account = "example-account"
+  policy  = "enforce"
+}
